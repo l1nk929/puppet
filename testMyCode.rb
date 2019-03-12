2fe8eab62c28caef57ba00b6f0f5614111511e07
link <lx17951@qq.com>
2019-03-12 15:35:13 +0800

    类继承

--- testMyCode.rb
+++ testMyCode.rb
@@ -1,30 +1,13 @@
-require "date"
-# def hello(name)
-#   print "hello,#{name}\n"
-# end
-# hello "link"
-# sayBye "link"
-#
-# days = Date.today-Date.new(1991,9,27)
-# p days.to_i
-# print days
-# $
-# a =
-# p a
-# puts "a:#{a}"
-a = [10,"abc",[1,2,3],Date.new(1990)]
-# unless a<9 then
-#   print a
-# end
-a.each do |item|
-  case item
-  when Numeric
-    puts "#{item} is Numeric"
-  when String
-    puts "#{item} is String"
-  when Array
-    puts "#{item} is Array"
-  else
-    puts "No Class."
-  end
-end
+require './testMySecondCode'
+
+# HelloWorld.hello
+# # HelloWorld.bye
+# # HelloWorld.say
+# # p HelloWorld::Chart
+# hello = HelloWorld.new
+# hello2 = HelloWorld.new
+# p hello.getCount
+# hello2.setCount(10)
+# p "".to_s
+a=MyHello.new("lniksnowolf")
+MyHello.bye("")
--- testMySecondCode.rb
+++ testMySecondCode.rb
@@ -1,6 +1,40 @@
-def sayBye(str)
-  print "Bye！！#{str}\n"
+class HelloWorld
+  Chart = "bazenga!"
+  def getChart
+    Chart
+  end
+  @@count = 1
+  attr_accessor :newname
+  def getCount
+    @@count
+  end
+  def setCount(count)
+    @@count=count
+  end
+  def initialize(newname)
+    @newname=newname
+  end
+  class << self
+    def bye(name:"link")
+      puts "bye,#{name}!"
+    end
+  end
 end
-p $x
-$x=1
-x=3
+
+class << HelloWorld
+  def hello(name:"link")
+    puts "hello,#{name}!"
+  end
+
+end
+
+def HelloWorld.say(name:"link")
+  puts "#{name} say hi~~"
+end
+
+class MyHello < HelloWorld
+
+  def initialize(*a)
+    super(*a)
+  end
+end
